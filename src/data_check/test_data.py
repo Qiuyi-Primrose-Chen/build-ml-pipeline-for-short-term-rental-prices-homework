2cb89c613fe507a997714054d2eeade9ecc7e98b
Qiuyi-Primrose-Chen <113934735+Qiuyi-Primrose-Chen@users.noreply.github.com>
2023-03-10 17:18:28 -0500

    updates

--- src/data_check/test_data.py
+++ src/data_check/test_data.py
@@ -29,22 +29,6 @@ def test_column_names(data):
     # This also enforces the same order
     assert list(expected_colums) == list(these_columns)
 
-def test_price_range(data, min_price, max_price):
-    """
-    Test if the price is between the range (min and max price)
-    """
-    idx = data['price'].between(min_price, max_price)
-    df_test = data[~idx].copy()
-
-    assert len(df_test) == 0
-
-
-def test_row_count(data):
-    """
-    Test the size of the dataset. It should not be too small or too large.
-    """
-    assert 15000 < data.shape[0] < 1000000
-
 
 def test_neighborhood_names(data):
 
@@ -79,3 +63,18 @@ def test_similar_neigh_distrib(data: pd.DataFrame, ref_data: pd.DataFrame, kl_th
 ########################################################
 # Implement here test_row_count and test_price_range   #
 ########################################################
+
+def test_row_count(data):
+    """
+    Test the size of the dataset. It should not be too small or too large.
+    """
+    assert 15000 < data.shape[0] < 1000000
+
+def test_price_range(data, min_price, max_price):
+    """
+    Test if the price is between the range (min and max price)
+    """
+    idx = data['price'].between(min_price, max_price)
+    df_test = data[~idx].copy()
+
+    assert len(df_test) == 0
